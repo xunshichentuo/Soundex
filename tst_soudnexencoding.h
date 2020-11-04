b53924540cab04e670c57b1583121e3c2619aae8
chentuo <chentuo@soonsolid.com>
2020-11-04 16:36:47 +0800

    add IgnoresNonAlphabetics test but passed

--- tst_soudnexencoding.h
+++ tst_soudnexencoding.h
@@ -39,3 +39,7 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
 {
     ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
