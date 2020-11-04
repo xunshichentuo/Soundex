5cb90bfb7a6f5b4ae289a938895f7940fd92128d
chentuo <chentuo@soonsolid.com>
2020-11-04 16:46:06 +0800

    add ReplacesMultipleConsonantsWithDigits test but core dump.

--- tst_soudnexencoding.h
+++ tst_soudnexencoding.h
@@ -40,6 +40,12 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
     ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
 
-TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
+{
     ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits)
+{
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
 }
