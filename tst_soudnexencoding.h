f2b0be932514ffa584eaf3584bf48cdab3f0b24b
chentuo <chentuo@soonsolid.com>
2020-11-04 16:59:01 +0800

    add IgnoresVowelLikeLetters test and passed

--- tst_soudnexencoding.h
+++ tst_soudnexencoding.h
@@ -53,3 +53,7 @@ TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits)
 TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
     ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
 }
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+    ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
